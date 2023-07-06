05a445c769f83affe6dc461c2df781035b32955a
Zachary Harrold <zac@harrold.com.au>
2023-07-06 22:15:23 +1000

    Attempting sorting of the tables

--- index.js
+++ index.js
@@ -601,6 +601,41 @@ async function showPlayerDetails(playerId) {
     gameDetails.showModal();
 }
 
+function sortTable(table, compare, column) {
+    const rows = table.rows;
+    let switching = true;
+    let shouldSwitch = false;
+
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+        // Start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /* Loop through all table rows (except the
+        first, which contains table headers): */
+        for (let i = 1; i < (rows.length - 1); i++) {
+            // Start by saying there should be no switching:
+            shouldSwitch = false;
+            /* Get the two elements you want to compare,
+            one from current row and one from the next: */
+            const x = rows[i].getElementsByTagName("td")[column];
+            const y = rows[i + 1].getElementsByTagName("td")[column];
+            // Check if the two rows should switch place:
+            if (compare(x, y)) {
+                // If so, mark as a switch and break the loop:
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            /* If a switch has been marked, make the switch
+            and mark that a switch has been done: */
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
+}
 
 
 
