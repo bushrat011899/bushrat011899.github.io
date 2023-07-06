8004092108babdccba194e4d18d8f1e198b42385
Zac Harrold <zac@harrold.com.au>
2023-07-06 10:19:25 +1000

    Fixes

--- index.js
+++ index.js
@@ -100,8 +100,6 @@ async function gameHash(teams) {
 async function storeGame(game) {
     const gameId = await gameHash(game);
 
-    console.log("Storing Game". gameId);
-
     const transaction = DB.transaction(["game", "team", "teamMember", "profile", "playerName", "playerMMR", "event"], "readwrite");
     const gamesStore = transaction.objectStore("game");
     const teamsStore = transaction.objectStore("team");
@@ -112,6 +110,14 @@ async function storeGame(game) {
     const eventsStore = transaction.objectStore("event");
 
     const stamp = Date.now();
+
+    const alreadyAdded = await new Promise((resolve) => {
+        const request = gamesStore.get(gameId);
+        request.onsuccess = () => resolve(true);
+        request.onerror = () => resolve(false);
+    });
+
+    if (alreadyAdded) return;
 
     gamesStore.put({
         id: gameId,
@@ -246,12 +252,13 @@ async function showGameDetails(gameId) {
         tableBody.removeChild(tableBody.lastChild);
     }
 
-    const transaction = DB.transaction(["game", "teamMember", "playerName", "playerMMR"], "readonly");
+    const transaction = DB.transaction(["game", "teamMember", "playerName", "playerMMR", "event"], "readonly");
 
     const gamesStore = transaction.objectStore("game");
     const teamMembersStore = transaction.objectStore("teamMember");
     const playerNamesStore = transaction.objectStore("playerName");
     const playerMMRsStore = transaction.objectStore("playerMMR");
+    const eventsStore = transaction.objectStore("event");
 
     const game = await new Promise((resolve, reject) => {
         gamesStore.get(gameId).onsuccess = (event) => {
@@ -271,14 +278,28 @@ async function showGameDetails(gameId) {
 
     gameDetails.querySelector("em").textContent = gameDate.toLocaleString();
 
+    const playerNameMapping = {};
+    let spannedRows = {};
+
     for (const teamMember of teamMembers) {
+        const teamSize = await new Promise((resolve, reject) => {
+            teamMembersStore.index("game_number").count([gameId, teamMember.number]).onsuccess = (event) => {
+                const result = event.target.result;
+                resolve(result);
+            };
+        });
+
         const row = document.createElement("tr");
         row.setAttribute("player", teamMember.profile);
         tableBody.append(row);
 
-        const teamEntry = document.createElement("td");
-        teamEntry.textContent = teamMember.number;
-        row.append(teamEntry);
+        if (!spannedRows[teamMember.number]) {
+            const teamEntry = document.createElement("td");
+            teamEntry.setAttribute("rowspan", teamSize);
+            teamEntry.textContent = teamMember.number;
+            row.append(teamEntry);
+            spannedRows[teamMember.number] = true;
+        }
 
         const playerNames = await new Promise((resolve, reject) => {
             playerNamesStore.index("profile").getAll(teamMember.profile).onsuccess = (event) => {
@@ -289,8 +310,10 @@ async function showGameDetails(gameId) {
 
         playerNames.sort((a, b) => b.date - a.date);
 
+        playerNameMapping[teamMember.profile] = playerNames[0].name;
+
         const nameEntry = document.createElement("td");
-        nameEntry.textContent = playerNames[0].name;
+        nameEntry.textContent = playerNameMapping[teamMember.profile];
         row.append(nameEntry);
 
         const playerMMR = await new Promise((resolve, reject) => {
@@ -303,6 +326,75 @@ async function showGameDetails(gameId) {
         const MMREntry = document.createElement("td");
         MMREntry.textContent = playerMMR.mmr;
         row.append(MMREntry);
+    }
+
+    const events = await new Promise((resolve, reject) => {
+        eventsStore.index("game").getAll(gameId).onsuccess = (event) => {
+            const result = event.target.result;
+            resolve(result);
+        };
+    });
+
+    events.sort((a, b) => a.clock - b.clock);
+
+    const eventsList = gameDetails.querySelector("ol[events]");
+
+    while (eventsList.firstChild) {
+        eventsList.removeChild(eventsList.lastChild);
+    }
+
+    for (const event of events) {
+        const entry = document.createElement("li");
+        eventsList.append(entry);
+
+        const seconds = event.clock % 60;
+        const minutes = (event.clock - seconds) / 60;
+        const clock = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+        const name = playerNameMapping[event.profile];
+
+        const categoryMap = {
+            "bountyextracted": "Bounty Extracted",
+            "bountypickedup": "Bounty Picked Up",
+            "downedbyme": "Downed By Me",
+            "downedbyteammate": "Downed By Teammate",
+            "downedme": "Downed Me",
+            "downedteammate": "Downed Teammate",
+            "killedbyme": "Killed By Me",
+            "killedbyteammate": "Killed By Teammate",
+            "killedme": "Killed Me",
+            "killedteammate": "Killed Teammate"
+        };
+
+        const category = categoryMap[event.category] ?? event.category;
+
+        const labelMap = {
+            "@ui_mmr_killed_hunter ~~@ui_team_details_killed": "Was Killed By Me",
+            "@ui_mmr_died_to_hunter ~~@ui_team_details_killed": "Killed Me",
+            "@ui_team_details_carried_bounty": "Picked Up Bounty",
+            "@ui_team_details_hunter_downed_teammate ~~@ui_team_details_downed": "Downed Teammate",
+            "@ui_team_details_downed": "Downed Teammate",
+            "@ui_team_details_hunter_downed_you ~~@ui_team_details_downed": "Downed Me",
+            "@ui_team_details_killed_by_team_mate ~~@ui_team_details_killed": "Was Killed By Teammate",
+            "@ui_team_details_killed_team_mate ~~@ui_team_details_downed": "Killed Teammate",
+            "@ui_team_details_killed": "Killed Teammate",
+            "@ui_team_details_teammate_downed_hunter ~~@ui_team_details_downed": "Was Downed By Teammate",
+            "@ui_team_details_you_downed_hunter ~~@ui_team_details_downed": "Was Downed By Me",
+            "@ui_team_details_extracted_bounty": "Extracted With Bounty"
+        };
+
+        const label = labelMap[event.label] ?? event.label;
+
+        const clockEntry = document.createElement("b");
+        entry.append(clockEntry);
+        clockEntry.textContent = clock;
+
+        const playerNameEntry = document.createElement("em");
+        entry.append(playerNameEntry);
+        playerNameEntry.textContent = name;
+
+        const detailsEntry = document.createElement("span");
+        entry.append(detailsEntry);
+        detailsEntry.textContent = label;
     }
 
     await new Promise((resolve, reject) => {
@@ -405,9 +497,22 @@ async function showPlayerDetails(playerId) {
     const gameDetails = document.querySelector("dialog#playerDetails");
     const mmrChartArea = gameDetails.querySelector("div#chartArea");
 
-    const transaction = DB.transaction(["playerMMR"], "readonly");
+    const transaction = DB.transaction(["playerMMR", "playerName"], "readonly");
 
     const playerMMRsStore = transaction.objectStore("playerMMR");
+    const playerNamesStore = transaction.objectStore("playerName");
+
+    const playerNames = await new Promise((resolve, reject) => {
+        playerNamesStore.index("profile").getAll(playerId).onsuccess = (event) => {
+            const result = event.target.result;
+            resolve(result);
+        };
+    });
+
+    playerNames.sort((a, b) => b.date - a.date);
+
+    gameDetails.querySelector("h2[name]").textContent = playerNames[0].name;
+    gameDetails.querySelector("p[profile]").textContent = playerId;
 
     const playerMMRs = await new Promise((resolve, reject) => {
         playerMMRsStore.index("profile").getAll(playerId).onsuccess = (event) => {
